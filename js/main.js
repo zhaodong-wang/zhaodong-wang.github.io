7666b059740a17e76bc1c67089ddfcca1b16ccf5
Zhaodong Wang <wd20060220@gmail.com>
2015-07-23 16:49:36 -0500

    Fixed menu problems.
    1. Add animation for the menu transition (opacity 0->1). Note: to
    change css sequentially, try to use .queue() function, and don’t forget
    to add .dequeue() at the end.
    2. There was an issue that when menu-icon is not at the top, click it,
    then the page auto scroll, but one would wait a long time to see the
    menu show up. The fix is simply add an if condition within hideMenu()
    function since this function would be called multiple times, which
    takes too much time.
    3. The last problem is, the webpage has a wired white space on right.
    To fix this, I found the trouble maker, which is the .meta class. The
    way to find it is:
    $(*).each(function(){
      rightPos = $(this).offset().left + $(this).width();
      // check the right position of every DOM element
     })

--- js/main.js
+++ js/main.js
@@ -39,123 +39,172 @@ function flipDiv(obj) {
 }
 
 var winHeight = $(window).height();
+var winWidth = $(window).width();
 
 var indexToLightUp;
 var indexToLightUpTemp;
 var matrixRegex = /matrix\((-?\d*\.?\d+),\s*0,\s*0,\s*(-?\d*\.?\d+),\s*0,\s*0\)/; // get the scale of element
 
-onscroll = function() {
-    var about_offset = $("#about").offset().top;
-    var edu_offset = $("#edu").offset().top;
-    var project_offset = $("#project").offset().top;
-    var pubs_offset = $("#pubs").offset().top;
-    var skill_offset = $("#skill").offset().top;
-    var contact_offset = $("#contact").offset().top;
-    var doc_offset = $(window).scrollTop() || $("body").scrollTop();
-    if (doc_offset >= about_offset) {
-        $('header').addClass('fixed');
-    }
-    else {
-        $('header').removeClass('fixed');
-        if ($(window).width() <= 850) hideMenu();
-    };
-
-    $('.circle .half.light').each(function(){
-        var contentOffSet = $(this).parent().parent().next().offset().top - $(window).scrollTop()
-        if (contentOffSet < 0.6 * winHeight &&
-            contentOffSet > 0) {
-            flipDiv($(this));
+$(function(){
+    $(window).scroll(function(){
+        var about_offset = $("#about").offset().top;
+        var edu_offset = $("#edu").offset().top;
+        var project_offset = $("#project").offset().top;
+        var pubs_offset = $("#pubs").offset().top;
+        var skill_offset = $("#skill").offset().top;
+        var contact_offset = $("#contact").offset().top;
+        var doc_offset = $(window).scrollTop() || $("body").scrollTop();
+        var aaa = $('header').height();
+        if (doc_offset >= about_offset) {
+            if (!$('header').hasClass('fixed')) {
+                $('header').addClass('fixed');
+            };
         }
-    });
+        else {
+            if ($('header').hasClass('fixed')) {
+                $('header').removeClass('fixed');
+            };
+            if ($(window).width() <= 850) hideMenu();
+        };
 
-    $('#about .pic, #about .profile, #edu .content, #project .content, #pubs .profile').each(function(){
-        if ($(this).offset().top - $(window).scrollTop() < 0.6 * winHeight &&
-            $(this).offset().top - $(window).scrollTop() > 0) {
-            $(this).removeClass('move-left');
-            $(this).removeClass('move-right');
-        }
-    });
-
-    var minDistance = Number.POSITIVE_INFINITY;
-
-    // get the index of closest item
-    $('.pub_group .pic').each(function(i){
-        var distTemp = Math.abs($(this).offset().top - $(window).scrollTop() - 0.5 * winHeight);
-        if (distTemp <= minDistance) {
-            minDistance = distTemp;
-            indexToLightUp = i;
-        }
-    });
-
-    $('.pub_group .pic').each(function(i){
-        var matches = $(this).css('webkitTransform').match(matrixRegex);
-        var scaleX = matches[1];
-        if (i == indexToLightUp) {
-            $(this).css({'background-color': '#eb6e1e'});
-            if ((typeof indexToLightUpTemp == 'undefined') ||
-                (indexToLightUpTemp != indexToLightUp)) {
-                if (scaleX == 1) {
-                    $(this).css({
-                        'transform': 'scale(1.02)',
-                        'mozTransform': 'scale(1.02)',
-                        'webkitTransform': 'scale(1.02)',
-                        'oTransform': 'scale(1.02)'
-                    });
-                };
-                indexToLightUpTemp = indexToLightUp;
+        $('.circle .half.light').each(function(){
+            var contentOffSet = $(this).parent().parent().next().offset().top - doc_offset
+            if (contentOffSet < 0.75 * winHeight &&
+                contentOffSet > 0.2 * winHeight) {
+                flipDiv($(this));
             }
-        } else {
-            $(this).css({'background-color': '#333'});
-            if (scaleX >= 1 && scaleX <= 1.02) {
-                $(this).css({
-                    'transform': 'scale(1)',
-                    'mozTransform': 'scale(1)',
-                    'webkitTransform': 'scale(1)',
-                    'oTransform': 'scale(1)'
-                });
+        });
+
+        $('#about .pic').each(function(){
+            var contentOffSet = $(this).next().offset().top - doc_offset
+            if (contentOffSet < 0.75 * winHeight &&
+                contentOffSet > 0.2 * winHeight) {
+                $(this).removeClass('move-left');
+            }
+        });
+
+        $('#about .pic, #about .profile, #edu .content, #project .content, #pubs .profile').each(function(){
+            if ($(this).offset().top - doc_offset < 0.75 * winHeight &&
+                $(this).offset().top - doc_offset > 0.2 * winHeight) {
+                $(this).removeClass('move-right');
+            }
+        });
+
+        if (pubs_offset - $(window).scrollTop() < 0.1 * winHeight) {
+            var minDistance = Number.POSITIVE_INFINITY;
+
+            // get the index of closest item
+            $('.pub_group .pic').each(function(i){
+                var distTemp = Math.abs($(this).offset().top - doc_offset - 0.5 * winHeight);
+                if (distTemp <= minDistance) {
+                    minDistance = distTemp;
+                    indexToLightUp = i;
+                }
+            });
+            // light up the item that is nearest to the center of the window
+            $('.pub_group .pic').each(function(i){
+                var matches = $(this).css('webkitTransform').match(matrixRegex);
+                var scaleX = matches[1];
+                if (i == indexToLightUp) {
+                    $(this).css({'background-color': '#eb6e1e'});
+                    if ((typeof indexToLightUpTemp == 'undefined') ||
+                        (indexToLightUpTemp != indexToLightUp)) {
+                        if (scaleX == 1) {
+                            $(this).css({
+                                'transform': 'scale(1.02)',
+                                'mozTransform': 'scale(1.02)',
+                                'webkitTransform': 'scale(1.02)',
+                                'oTransform': 'scale(1.02)'
+                            });
+                        };
+                        indexToLightUpTemp = indexToLightUp;
+                    }
+                } else {
+                    $(this).css({'background-color': '#333'});
+                    if (scaleX >= 1 && scaleX <= 1.02) {
+                        $(this).css({
+                            'transform': 'scale(1)',
+                            'mozTransform': 'scale(1)',
+                            'webkitTransform': 'scale(1)',
+                            'oTransform': 'scale(1)'
+                        });
+                    }
+                }
+            });
+        }
+
+
+        if (skill_offset - doc_offset < 0.2 * winHeight &&
+            skill_offset - doc_offset > - 0.2 * winHeight) {
+            for (var i = 0; i < 6; i++) {
+                $("#skill" + i).addClass(" stroke" + i);
             }
         }
+        var xxx = $('header').width();
     });
+});
 
-    if (doc_offset >= skill_offset - 0.3 * about_offset) {
-        for (var i = 0; i < 6; i++) {
-            $("#skill" + i).addClass(" stroke" + i);
-        }
-    }
-};
+// onscroll = function() {
+
+// };
 
 /*-----------------------------------------------------------------------------------*/
 /*  Menu effects
 /*-----------------------------------------------------------------------------------*/
 
 function hideMenu() {
-    $('nav ul, nav:active ul').css({'height': winHeight, 'display': 'none'});
-    $('.bg-ultrawhite, .bg-mesh, .bg-white, #footer').css({'webkitFilter': 'blur(0)'});
+    if ($('.bg-ultrawhite').css('webkitFilter') == 'blur(2px)') {
+        $('nav ul, nav:active ul').css({'display': 'none', 'opacity': 0});
+        $('.bg-ultrawhite, .bg-mesh, .bg-white, #footer').css({'webkitFilter': 'blur(0)'});
+    }
 }
 
 function toggleMenu() {
-    if ($('nav ul').css('display') == 'block') {
-        $('nav ul, nav:active ul').css({'height': winHeight, 'display': 'none'});
-        $('.bg-ultrawhite, .bg-mesh, .bg-white, #footer').css({'webkitFilter': 'blur(0)'});
+    if ($('.bg-ultrawhite').css('webkitFilter') != 'blur(2px)') {
+        $('nav ul, nav:active ul')
+            .queue(function(){
+                $(this).css({'display': 'block'}).dequeue();
+            })
+            .queue(function(){
+                $(this).animate({'opacity': 1}, 300, 'easeOutCubic').dequeue();
+            });
+        $('.bg-ultrawhite, .bg-mesh, .bg-white, #footer').css({'webkitFilter': 'blur(2px)'});
     }
     else {
-        $('nav ul, nav:active ul').css({'height': winHeight, 'display': 'block'});
-        $('.bg-ultrawhite, .bg-mesh, .bg-white, #footer').css({'webkitFilter': 'blur(2px)'});
+        $('nav ul, nav:active ul')
+            .queue(function(){
+                $(this).animate({'opacity': 0}, 300, 'easeOutCubic').dequeue();
+            })
+            .queue(function(){
+                $(this).css({'display': 'none'}).dequeue();
+            });
+        $('.bg-ultrawhite, .bg-mesh, .bg-white, #footer').css({'webkitFilter': 'blur(0)'});
     }
 }
 
 $(document).ready(function(){
+    var menuHeight = winHeight - 60;
+    if (winWidth < 850) {
+        $('nav ul, nav:active ul').css({'width': winWidth, 'height': menuHeight, 'display': 'none', 'opacity': 0});
+        $('#menu-bg').css({'width': winWidth});
+    }
+
     $('#about .pic').addClass('move-left');
     $('#about .profile, #edu .content, #project .content, #pubs .profile').addClass('move-right');
     $('#menu-icon').click(function(){
-        var yPos = $(window).scrollTop();
+        var doc_offset = $(window).scrollTop() || $("body").scrollTop();
         var about_offset = $('#about').offset().top;
-        if (yPos < about_offset) {
-            $('html, body').animate({
+        if (doc_offset <= about_offset) {
+            $('html body').animate({
                     scrollTop: $("#about").offset().top
-                }, 500, toggleMenu);
+                }, 500, function(){
+                    setTimeout(toggleMenu, 500);
+                });
         }
         else toggleMenu();
+    });
+    $('#nextInHome').click(function(){
+        move('about',0);
     });
 });
 
