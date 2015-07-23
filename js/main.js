f324c5eccfb9399f99d928dd8d3b6ee6af25ccfc
Zhaodong Wang <wd20060220@gmail.com>
2015-07-23 01:28:29 -0500

    shit!!!

--- js/main.js
+++ js/main.js
@@ -159,13 +159,6 @@ $(document).ready(function(){
         transform: 'translateX(80%)',
         opacity: 0
     });
-    // $('#about .profile, #edu .content, #project .content, #pubs .profile').css({
-    //     webkitTransform: 'translateX(80%)',
-    //     mozTransform: 'translateX(80%)',
-    //     oTransform: 'translateX(80%)',
-    //     transform: 'translateX(80%)',
-    //     opacity: 0
-    // });
     $('#menu-icon').click(function(){
         var yPos = $(window).scrollTop();
         var about_offset = $('#about').offset().top;
